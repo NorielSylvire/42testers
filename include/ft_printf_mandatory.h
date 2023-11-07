4968d0d0888e31371622452c4508a7e99738e4d5
Flaviu Emanuel Hongu <fhongu@c3r2s5.42madrid.com>
2023-11-07 18:49:57 +0100

    wtf

---
+++ include/ft_printf_mandatory.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_mandatory.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhongu <fhongu@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 18:40:43 by fhongu            #+#    #+#             */
+/*   Updated: 2023/11/07 18:49:47 by fhongu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_MANDATORY_H
+# define FT_PRINTF_MANDATORY_H
+
+// @TODO PLEASE change this absolute path to something better ASAPP!!
+#include "/Users/fhongu/documents/repos/ft_printf/include/ft_printf.h"
+
+void test_c();:w
+
+
+#endif:wq
+
